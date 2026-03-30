b84546bc559606c19a04988329718c11ed26c935
sneha-jagtap-patil <sapatil2115@gmail.com>
2026-03-30 13:46:59 +0530

    update file

--- app.py
+++ app.py
@@ -1,3 +1,14 @@
-from books_recommender.logger import log
+# from books_recommender.logger import log
 
-log.logging.info("Starting the books recommender system application.")
+# log.logging.info("Starting the books recommender system application.")
+
+import os
+import sys
+import pickle
+import streamlit as st
+import numpy as np
+from books_recommender.logger.log import logging
+from books_recommender.config.configuration import AppConfiguration
+from books_recommender.pipeline.training_pipeline import TrainingPipeline
+from books_recommender.exception.exception_handler import AppException
+
