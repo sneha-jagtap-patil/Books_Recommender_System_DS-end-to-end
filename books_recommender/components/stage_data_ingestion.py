3a29824b1c7ff03425b13573ed8caf263d7e5c97
sneha jagtap patil <sapatil2115@gmail.com>
2026-03-31 14:29:12 +0530

    updare files

---
+++ books_recommender/components/stage_data_ingestion.py
@@ -0,0 +1,7 @@
+import os
+import sys
+from six.moves import urllib
+import zipfile
+from books_recommender.logger.log import logging
+from books_recommender.exception.exception_handler import AppException
+from books_recommender.config.configuration import AppConfiguration
