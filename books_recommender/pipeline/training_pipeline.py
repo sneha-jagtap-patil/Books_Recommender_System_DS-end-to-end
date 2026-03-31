d558a4bae670367bbe5ac5cd29c6cfa881b23a6a
sneha jagtap patil <sapatil2115@gmail.com>
2026-03-31 15:56:30 +0530

    updare files

--- books_recommender/pipeline/training_pipeline.py
+++ books_recommender/pipeline/training_pipeline.py
@@ -13,12 +13,12 @@ class TrainingPipeline:
         self.model_trainer = ModelTrainer()
 
     
-    # def start_training_pipeline(self):
-    #     """
-    #     Starts the training pipeline
-    #     :return: none
-    #     """
-    #     self.data_ingestion.initiate_data_ingestion()
-    #     self.data_validation.initiate_data_validation()
-    #     self.data_transformation.initiate_data_transformation()
-    #     self.model_trainer.initiate_model_trainer() 
+    def start_training_pipeline(self):
+        """
+        Starts the training pipeline
+        :return: none
+        """
+        self.data_ingestion.initiate_data_ingestion()
+        self.data_validation.initiate_data_validation()
+        self.data_transformation.initiate_data_transformation()
+        self.model_trainer.initiate_model_trainer() 
