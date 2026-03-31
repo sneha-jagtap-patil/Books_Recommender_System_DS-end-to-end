c62f6caa702e95a275a46ee99610bc8ccffc16cc
sneha jagtap patil <sapatil2115@gmail.com>
2026-03-31 16:00:18 +0530

    updare files

--- books_recommender/components/stage_data_transformation.py
+++ books_recommender/components/stage_data_transformation.py
@@ -18,41 +18,41 @@ class DataValidation:
 
 
     
-    # def preprocess_data(self):
-    #     try:
-    #         ratings = pd.read_csv(self.data_validation_config.ratings_csv_file, sep=";", error_bad_lines=False, encoding='latin-1')
-    #         books = pd.read_csv(self.data_validation_config.books_csv_file, sep=";", error_bad_lines=False, encoding='latin-1')
+    def preprocess_data(self):
+        try:
+            ratings = pd.read_csv(self.data_validation_config.ratings_csv_file, sep=";", error_bad_lines=False, encoding='latin-1')
+            books = pd.read_csv(self.data_validation_config.books_csv_file, sep=";", error_bad_lines=False, encoding='latin-1')
             
-    #         logging.info(f" Shape of ratings data file: {ratings.shape}")
-    #         logging.info(f" Shape of books data file: {books.shape}")
+            logging.info(f" Shape of ratings data file: {ratings.shape}")
+            logging.info(f" Shape of books data file: {books.shape}")
 
-    #         #Here Image URL columns is important for the poster. So, we will keep it
-    #         books = books[['ISBN','Book-Title', 'Book-Author', 'Year-Of-Publication', 'Publisher','Image-URL-L']]
-    #         # Lets remane some wierd columns name in books
-    #         books.rename(columns={"Book-Title":'title',
-    #                             'Book-Author':'author',
-    #                             "Year-Of-Publication":'year',
-    #                             "Publisher":"publisher",
-    #                             "Image-URL-L":"image_url"},inplace=True)
+            #Here Image URL columns is important for the poster. So, we will keep it
+            books = books[['ISBN','Book-Title', 'Book-Author', 'Year-Of-Publication', 'Publisher','Image-URL-L']]
+            # Lets remane some wierd columns name in books
+            books.rename(columns={"Book-Title":'title',
+                                'Book-Author':'author',
+                                "Year-Of-Publication":'year',
+                                "Publisher":"publisher",
+                                "Image-URL-L":"image_url"},inplace=True)
 
             
-    #         # Lets remane some wierd columns name in ratings
-    #         ratings.rename(columns={"User-ID":'user_id',
-    #                             'Book-Rating':'rating'},inplace=True)
+            # Lets remane some wierd columns name in ratings
+            ratings.rename(columns={"User-ID":'user_id',
+                                'Book-Rating':'rating'},inplace=True)
 
-    #         # Lets store users who had at least rated more than 200 books
-    #         x = ratings['user_id'].value_counts() > 200
-    #         y = x[x].index
-    #         ratings = ratings[ratings['user_id'].isin(y)]
+            # Lets store users who had at least rated more than 200 books
+            x = ratings['user_id'].value_counts() > 200
+            y = x[x].index
+            ratings = ratings[ratings['user_id'].isin(y)]
 
-    #         # Now join ratings with books
-    #         ratings_with_books = ratings.merge(books, on='ISBN')
-    #         number_rating = ratings_with_books.groupby('title')['rating'].count().reset_index()
-    #         number_rating.rename(columns={'rating':'num_of_rating'},inplace=True)
-    #         final_rating = ratings_with_books.merge(number_rating, on='title')
+            # Now join ratings with books
+            ratings_with_books = ratings.merge(books, on='ISBN')
+            number_rating = ratings_with_books.groupby('title')['rating'].count().reset_index()
+            number_rating.rename(columns={'rating':'num_of_rating'},inplace=True)
+            final_rating = ratings_with_books.merge(number_rating, on='title')
 
-    #         # Lets take those books which got at least 50 rating of user
-    #         final_rating = final_rating[final_rating['num_of_rating'] >= 50]
+            # Lets take those books which got at least 50 rating of user
+            final_rating = final_rating[final_rating['num_of_rating'] >= 50]
 
     #         # lets drop the duplicates
     #         final_rating.drop_duplicates(['user_id','title'],inplace=True)
