91684d0dccad6f3d7c2aeee155d33496da398e13
sneha jagtap patil <sapatil2115@gmail.com>
2026-03-31 14:53:17 +0530

    updare files

--- books_recommender/pipeline/training_pipeline.py
+++ books_recommender/pipeline/training_pipeline.py
@@ -5,20 +5,20 @@ from books_recommender.components.stage_03_model_trainer import ModelTrainer
 
 
 
-# class TrainingPipeline:
-#     def __init__(self):
-#         self.data_ingestion = DataIngestion()
-#         self.data_validation = DataValidation()
-#         self.data_transformation = DataTransformation()
-#         self.model_trainer = ModelTrainer()
+class TrainingPipeline:
+    def __init__(self):
+        self.data_ingestion = DataIngestion()
+        self.data_validation = DataValidation()
+        self.data_transformation = DataTransformation()
+        self.model_trainer = ModelTrainer()
 
     
-#     def start_training_pipeline(self):
-#         """
-#         Starts the training pipeline
-#         :return: none
-#         """
-#         self.data_ingestion.initiate_data_ingestion()
-#         self.data_validation.initiate_data_validation()
-#         self.data_transformation.initiate_data_transformation()
-#         self.model_trainer.initiate_model_trainer() 
+    # def start_training_pipeline(self):
+    #     """
+    #     Starts the training pipeline
+    #     :return: none
+    #     """
+    #     self.data_ingestion.initiate_data_ingestion()
+    #     self.data_validation.initiate_data_validation()
+    #     self.data_transformation.initiate_data_transformation()
+    #     self.model_trainer.initiate_model_trainer() 
