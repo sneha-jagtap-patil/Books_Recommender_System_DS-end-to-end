669544b8bf68694069de277e6904d266883ae3fa
sneha jagtap patil <sapatil2115@gmail.com>
2026-03-31 14:31:22 +0530

    updare files

--- books_recommender/components/stage_data_ingestion.py
+++ books_recommender/components/stage_data_ingestion.py
@@ -5,3 +5,17 @@ import zipfile
 from books_recommender.logger.log import logging
 from books_recommender.exception.exception_handler import AppException
 from books_recommender.config.configuration import AppConfiguration
+
+class DataIngestion:
+
+    def __init__(self, app_config = AppConfiguration()):
+        """
+        DataIngestion Intialization
+        data_ingestion_config: DataIngestionConfig 
+        """
+        try:
+            logging.info(f"{'='*20}Data Ingestion log started.{'='*20} ")
+            self.data_ingestion_config= app_config.get_data_ingestion_config()
+        except Exception as e:
+            raise AppException(e, sys) from e
+        
