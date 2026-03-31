83469b46ae62ad67a6a62213957181b45e69a1d4
sneha jagtap patil <sapatil2115@gmail.com>
2026-03-31 16:14:04 +0530

    updare files

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
 from books_recommender.pipeline.training_pipeline import TrainingPipeline
 
 obj = TrainingPipeline()
-<<<<<<< HEAD
 obj.start_training_pipeline()
-=======
-obj.start_training_pipeline()
->>>>>>> 8d068c4 (updare files)
