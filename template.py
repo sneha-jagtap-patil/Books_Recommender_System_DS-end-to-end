9a9700d17c8677d347d0bae2de41a55d3aed9f0e
sneha jagtap patil <sapatil2115@gmail.com>
2026-04-06 21:34:07 +0530

    update file

--- template.py
+++ template.py
@@ -4,7 +4,7 @@ import logging
 
 logging.basicConfig(level=logging.INFO, format='[%(asctime)s]: %(message)s:')
 
-#..................
+
 project_name = "books_recommender_System" 
 
 list_of_files = [
